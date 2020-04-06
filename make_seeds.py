320816c6e4ff52a7bafd5ccdb74478aeb48244a8
procfs <wireless@kaist.ac.kr>
2020-04-06 00:47:19 -0700

    fixed the fuzzer a bit

--- make_seeds.py
+++ make_seeds.py
@@ -9,12 +9,21 @@ if len(sys.argv) != 2:
 pardir = sys.argv[1]
 
 fid = 0
+types = []
+globs = globals()
+
 for x in sys.modules:
 	for y in sys.modules[x].__dict__:
 		with open("%s/seed%d"%(pardir, fid), "wb") as f:
 			try:
-				marshal.dump(sys.modules[x].__dict__[y], f)
-				fid += 1
+				obj = sys.modules[x].__dict__[y]
+				if type(obj) in types:
+					continue
+				else:
+					marshal.dump(obj, f)
+					types.append(type(obj))
+					fid += 1
 			except ValueError:
 				pass
-			
+
+print("created %d seeds, with %d unique types"%(fid, len(types)))
--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname $0)
 user=fuzz
 IMAGENAME=marshalfuzz
-rm -rf work
-mkdir -p work
-sudo docker build -t $IMAGENAME .
-sudo docker run -it --mount "type=bind,source=$(pwd)/work,target=/home/$user/work" $IMAGENAME /bin/bash
+rm -rf output
+mkdir -p output
+#sudo docker build -t $IMAGENAME .
+sudo docker run -it --mount "type=bind,source=$(pwd)/output,target=/home/$user/output" $IMAGENAME
